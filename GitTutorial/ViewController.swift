473918549f304dbf9a9921b35452340b86b1eb22
Rui Cao <rayray199085@gmail.com>
2019-06-16 09:32:45 +1000

    Initial Commit

---
+++ GitTutorial/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitTutorial
+//
+//  Created by Stephen Cao on 16/6/19.
+//  Copyright © 2019 Stephencao Cao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
